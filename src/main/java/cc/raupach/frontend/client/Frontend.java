cb1d28bcb5eed7021ff12967f660092ad16a9f67
terst <terst@terst.fritz.box>
2012-10-21 10:14:52 +0200

    refactor html

--- src/main/java/cc/raupach/frontend/client/Frontend.java
+++ src/main/java/cc/raupach/frontend/client/Frontend.java
@@ -1,6 +1,9 @@
 package cc.raupach.frontend.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * @author Oliver Raupach, 20.10.2012
@@ -11,6 +14,14 @@ public class Frontend implements EntryPoint
    @Override
    public void onModuleLoad()
    {
+      
+      Button sendButton = new Button("Start");
+      Label nameField = new Label();
+      nameField.setText("0");
+      
+      
+      RootPanel.get("nameFieldContainer").add(nameField);
+      RootPanel.get("sendButtonContainer").add(sendButton);
    }
 
 }
